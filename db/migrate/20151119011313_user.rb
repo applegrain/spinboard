8ecc6079bd4110f90010ed9dbb13df5b055757df
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-11-18 18:31:57 -0700

    Migrate users and add user model a user has a username, password (bcrypt) and email validates: presence of all might have emails and/or usernames be unique

---
+++ db/migrate/20151119011313_user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
+    end
+  end
+end
